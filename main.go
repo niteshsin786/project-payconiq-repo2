11dd0828f257eb913f07de82b98b89585cb716ff
niteshsin786 <61456435+niteshsin786@users.noreply.github.com>
2021-06-15 14:01:32 +0200

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World Repo2"}
+		res := &response{Message: "Hello World Repo2 edit 1"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
